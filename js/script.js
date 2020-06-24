a326cd0d8181029ea9c4635de849878bed63c4b0
semenova-lv <lyudmila.klyueva@gmail.com>
2020-06-24 20:04:47 +0400

    завершаем верстку

--- js/script.js
+++ js/script.js
@@ -1,10 +1,18 @@
 var ButtonSearch = document.querySelector(".search");
-var SearchPopup = document.querySelector(".appoinment-form");
-var DateArrival = document.querySelector(".date-arrival-choice");
+var SearchForm = document.querySelector(".appoinment-form");
+var DateArrival = SearchForm.querySelector("[name=date-arrival]");
+var DateDeparture = SearchForm.querySelector("[name=date-departure]");
 
-
-ButtonSearch.addEventListener("click", function ()
-{SearchPopup.classList.toggle("appoinment-form-hidden");
-SearchPopup.classList.toggle("appoinment-form");
-DateArrival.focus();
+ButtonSearch.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    SearchForm.classList.toggle("appoinment-form-hidden");
+    SearchForm.classList.toggle("appoinment-form");
+    SearchForm.classList.remove("appoinment-form-error");
+    DateArrival.focus();
 });
+
+SearchForm.addEventListener("submit", function (evt) {
+    if (!DateArrival.value || !DateDeparture.value) {
+    evt.preventDefault();
+    SearchForm.classList.add("appoinment-form-error");}
+  });
